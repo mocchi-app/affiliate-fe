8ec1179b12141c41a3567ac09e9a60327c34ae64
donald <donaldterryprofess@hotmail.com>
2020-08-04 20:33:23 +0200

    Replace icons on landing page

--- pages/index.js
+++ pages/index.js
@@ -32,19 +32,19 @@ export default function AffiliatePage() {
         <div className={styles.cards}>
           <div className={styles.card}>
             <div className={styles.imgContainer}>
-              <img src='/images/gift.png' alt='' />
+              <img src='/images/inventory.png' alt='' />
             </div>
             <p className={styles.desc}>Review products from brands.</p>
           </div>
           <div className={styles.card}>
             <div className={styles.imgContainer}>
-              <img src='/images/link.png' alt='' />
+              <img src='/images/shoppable.png' alt='' />
             </div>
             <p className={styles.desc}>Share the products you love.</p>
           </div>
           <div className={styles.card}>
             <div className={styles.imgContainer}>
-              <img src='/images/dollar-sign.png' alt='' />
+              <img src='/images/order.png' alt='' />
             </div>
             <p className={styles.desc}>Earn 10% from brands on each sale.</p>
           </div>
